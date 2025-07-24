c009df78dc18289f6a95e95ec5257e6b7d785e06
chanikawongyakuem <chanika2203@gmail.com>
2025-07-24 14:19:13 +0700

    create

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/username/myawesomepkg
+
+go 1.22.3
---
+++ mywesomepkg.go
@@ -0,0 +1,10 @@
+package packge_golang_
+
+import "strings"
+
+// ToUpperCase แปลงข้อความให้เป็นตัวพิมพ์ใหญ่
+func ToUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+// ToLowerCase แปลงข้อความให้เป็นตัวพิมพ์เล็ก
