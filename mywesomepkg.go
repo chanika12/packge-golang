bc4956bd21f48bb7855856b45808f741a727c1b2
chanikawongyakuem <chanika2203@gmail.com>
2025-07-24 15:09:04 +0700

    create

--- mywesomepkg.go
+++ mywesomepkg.go
@@ -1,8 +1,14 @@
 package packge_golang
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // ToUpperCase แปลงข้อความให้เป็นตัวพิมพ์ใหญ่
 func ToUpperCase(s string) string {
 	return strings.ToUpper(s)
+}
+func TestCha(s string) string {
+	return fmt.Sprintf("Hello, %s!\n", s)
 }
